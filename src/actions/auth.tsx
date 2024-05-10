f44116fd0182a26aed91714ee45afbf82c245a02
Ghulam Rasool <grsaddiqui75@gmail.com>
2024-05-10 17:42:06 +0500

    auth with cookies

--- src/actions/auth.tsx
+++ src/actions/auth.tsx
@@ -3,11 +3,21 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const handleSubmit = async (e: FormData) => {
+  // try {
   const email = e.get("email");
   const password = e.get("password");
   const auth = { email, password };
-  cookies().set("auth", JSON.stringify(auth));
+
+  var oneday = 24 * 60 * 60 * 1000;
+  var exp = Date.now() + oneday;
+  cookies().set("auth", JSON.stringify(auth), {
+    expires: new Date(exp),
+  });
+
   redirect("/");
+  // } catch (error) {
+  //   console.log("some error", error);
+  // }
 };
 
 export const logoutAction = () => {
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,21 +3,30 @@ import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-
   const cookiesStore = cookies();
   const authValues = cookiesStore.get("auth")?.value;
 
   const accessAble =
     pathname === "/admin/login" || pathname === "/admin/register";
 
-  if (accessAble) {
-    if (authValues) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  } else {
-    if (!authValues) {
-      return NextResponse.redirect(new URL("/admin/login", request.url));
-    }
+  // is working fine 👌🏻👌🏻👌🏻👌🏻
+  // if (accessAble) {
+  //   if (authValues) {
+  //     return NextResponse.redirect(new URL("/", request.url));
+  //   }
+  // } else {
+  //   if (!authValues) {
+  //     return NextResponse.redirect(new URL("/admin/login", request.url));
+  //   }
+  // }
+
+  // this one is also working.. fine
+
+  if (accessAble && authValues) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  if (!accessAble && !authValues) {
+    return NextResponse.redirect(new URL("/admin/login", request.url));
   }
 }
 
